21744560cb5bf66ef03c21fb6c84d7f93a83cb76
Jay <jay.sanjay.karia@gmail.com>
2021-08-18 11:54:39 +0530

    Still Working on decode() :(((

--- Password Manager/src/Version2/password.java
+++ Password Manager/src/Version2/password.java
@@ -55,7 +55,7 @@ public class password {
                         Scanner security = new Scanner(System.in);
                         String key = security.nextLine();
 
-                        String[] originalValues = {n, message, password, key};
+                        String[] originalValues = {n, message, password, key, cDir};
                         /*Setting a Password*/
                         encode(originalValues);
                     } else {
@@ -123,7 +123,7 @@ public class password {
         String fK = fKey.nextLine();
 
         // Make a File Object
-        File file = new File(originalValues[3] + fK + ".txt");
+        File file = new File(originalValues[4] + fK + ".txt");
 
 
         // Write into a File
@@ -146,6 +146,28 @@ public class password {
     }
 
     public static void decode(String[] originalValues, String[] fakeValues) {
-        
+        String oName, oMessage, oPass, oKey;
+        String fName, fMessage, fPass, fKey;
+
+        oName = originalValues[0];
+        oMessage = originalValues[1];
+        oPass = originalValues[2];
+        oKey = originalValues[3];
+
+        fName = fakeValues[0];
+        fMessage= fakeValues[1];
+        fPass = fakeValues[2];
+        fKey = fakeValues[3];
+
+        String directory = originalValues[4];
+
+        // Writing Fake values
+        File file = new File(directory + fKey + ".txt");
+        try {
+            Writer writer = new FileWriter(file);
+            writer.write("Name: " + oName + "\nMessage: " + oMessage + "\nPassword: " + oPass + "\nSecurity Key: " + oKey);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
